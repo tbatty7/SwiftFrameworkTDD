1676f536d0c7ec1b504e8e59e25b21f04de7594e
Tim Batty <tbatty7@gmail.com>
2022-03-03 12:10:58 -0500

    Refactored test

--- SwiftFrameworkTDDTests/GreeterTests.swift
+++ SwiftFrameworkTDDTests/GreeterTests.swift
@@ -2,14 +2,18 @@
 import XCTest
 
 final class GreeterTests: XCTestCase {
-
+    
     func test_greet_with1159am_shouldSayGoodMorning() throws {
-        let greeter = Greeter(name: "")
-        let components = DateComponents(calendar: Calendar.current, hour: 11, minute: 59)
-        let time = components.date!
+        let greeter = Greeter(name: "")       
+        let time = date(hour: 11, minute: 59)
         
         let result: String = greeter.greet(time: time)
         
         XCTAssertEqual(result, "Good morning.")
+    }
+    
+    private func date(hour: Int, minute: Int) -> Date {
+        let components = DateComponents(calendar: Calendar.current, hour: hour, minute: minute)
+        return components.date!
     }
 }
