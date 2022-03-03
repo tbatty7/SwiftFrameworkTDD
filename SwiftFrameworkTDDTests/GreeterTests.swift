5f31d54950c8f88367ed707032ce95aadb8901ae
Tim Batty <tbatty7@gmail.com>
2022-03-03 11:58:57 -0500

    Added date with time to test in order to pass it into method

--- SwiftFrameworkTDDTests/GreeterTests.swift
+++ SwiftFrameworkTDDTests/GreeterTests.swift
@@ -5,5 +5,7 @@ final class GreeterTests: XCTestCase {
 
     func test_greet_with1159am_shouldSayGoodMorning() throws {
         let greeter = Greeter(name: "")
+        let components = DateComponents(calendar: Calendar.current, hour: 11, minute: 59)
+        let time = components.date!
     }
 }
