1e6dce6f9a10ef6aaee0ed5aa08fae85631be6fe
Tim Batty <tbatty7@gmail.com>
2022-03-03 12:14:51 -0500

    Refactored test more to reduce clutter

--- SwiftFrameworkTDDTests/GreeterTests.swift
+++ SwiftFrameworkTDDTests/GreeterTests.swift
@@ -4,10 +4,9 @@ import XCTest
 final class GreeterTests: XCTestCase {
     
     func test_greet_with1159am_shouldSayGoodMorning() throws {
-        let greeter = Greeter(name: "")       
-        let time = date(hour: 11, minute: 59)
+        let greeter = Greeter(name: "")
         
-        let result: String = greeter.greet(time: time)
+        let result = greeter.greet(time: date(hour: 11, minute: 59))
         
         XCTAssertEqual(result, "Good morning.")
     }
