b05c7f9c967c38be42c1028101656e004589c391
Tim Batty <tbatty7@gmail.com>
2022-03-03 11:39:32 -0500

    Initial Commit

---
+++ SwiftFrameworkTDD/SwiftFrameworkTDD.h
@@ -0,0 +1,18 @@
+//
+//  SwiftFrameworkTDD.h
+//  SwiftFrameworkTDD
+//
+//  Created by Timothy D Batty on 3/3/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftFrameworkTDD.
+FOUNDATION_EXPORT double SwiftFrameworkTDDVersionNumber;
+
+//! Project version string for SwiftFrameworkTDD.
+FOUNDATION_EXPORT const unsigned char SwiftFrameworkTDDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFrameworkTDD/PublicHeader.h>
+
+
